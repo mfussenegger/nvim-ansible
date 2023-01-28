38a9d60d13d8b3c54f910b039c82b6ee2ac6562b
Mathias Fussenegger <f.mathias@zignar.net>
2023-01-28 13:11:44 +0100

    Fix keywordprg definition

--- ftplugin/ansible.lua
+++ ftplugin/ansible.lua
@@ -1,6 +1,6 @@
 local api = vim.api
 if vim.fn.executable('ansible-doc') then
-  vim.bo.keywordprg = 'sp term://ansible-doc'
+  vim.bo.keywordprg = ':sp term://ansible-doc'
 end
 local fname = api.nvim_buf_get_name(0)
 if fname:find('tasks/') then

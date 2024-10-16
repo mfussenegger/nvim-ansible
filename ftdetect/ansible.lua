e6967282aa41455f94c51df971c55aebd2fe4fed
Marvin Velàsquez <mvelasquez@paypal.com>
2024-10-16 07:42:27 -0600

    feat(ansible.lua): add ft support for molecule Ansible's test suite

--- ftdetect/ansible.lua
+++ ftdetect/ansible.lua
@@ -9,6 +9,7 @@ if vim.filetype then
       [".*/roles/.*/tasks/.*%.ya?ml"] = "yaml.ansible",
       [".*/roles/.*/handlers/.*%.ya?ml"] = "yaml.ansible",
       [".*/tasks/.*%.ya?ml"] = "yaml.ansible",
+      [".*/molecule/.*%.ya?ml"] = "yaml.ansible",
     },
   })
 else
@@ -30,6 +31,8 @@ else
       "*/roles/*/handlers/*.yaml",
       "*/tasks/*.yml",
       "*/tasks/*.yaml",
+      "*/molecule/*.yml",
+      "*/molecule/*.yaml",
     },
     callback = function()
       vim.bo.filetype = "yaml.ansible"
